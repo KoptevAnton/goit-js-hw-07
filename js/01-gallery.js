5ebab92f9881d6b9a92594791c4fe26778702c15
KoptevAnton <koptev110593@gmail.com>
2023-09-05 23:22:13 +0300

    hw 7 js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryContainer = document.querySelector('.gallery');
+
+const createGalleryItemMarkup = ({ preview, original, description }) => {
+  return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>
+  `;
+};
+
+const galleryMarkup = galleryItems.map(createGalleryItemMarkup).join('');
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+// Реалізація делегування та відкриття модального вікна
+const modal = basicLightbox.create('<img src="" width="800" height="600">');
+const galleryImages = document.querySelectorAll('.gallery__image');
+
+galleryContainer.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  if (event.target.classList.contains('gallery__image')) {
+    const originalImageSrc = event.target.dataset.source;
+    modal.element().querySelector('img').src = originalImageSrc;
+    modal.show();
+  }
+});
+
+// Закриття модального вікна за допомогою клавіші Escape
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape') {
+    modal.close();
+  }
+});
+
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const gallery = document.querySelector('.gallery');
+
+// Функція для створення і рендерування розмітки галереї
+function createGalleryItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              alt="${description}"
+            />
+          </a>
+        </li>
+      `;
+    })
+    .join('');
+}
+
+// Додаємо розмітку до списку .gallery
+gallery.insertAdjacentHTML('beforeend', createGalleryItemsMarkup(galleryItems));
+
+// Ініціалізація бібліотеки SimpleLightbox
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
 console.log(galleryItems);
